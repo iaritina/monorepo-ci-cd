642244f6fe1fd66b217261f909e1ef74bb76da1d
iaritina <iaritina7@gmail.com>
2025-12-26 11:10:25 +0300

    test CI/CD

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ status: "ok", uptime: process.uptime() });
+  res.json({
+    status: "ok",
+    message: "Hello, Server!",
+    uptime: process.uptime(),
+  });
 });
 
 module.exports = app;
@@ -11,6 +15,6 @@ module.exports = app;
 if (require.main === module) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Hello, Server listening on http://localhost:${port}`);
   });
 }
