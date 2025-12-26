7f0ef5b4445a21570ddd350c06d641a2f54c7c89
iaritina <iaritina7@gmail.com>
2025-12-26 11:24:00 +0300

    update message

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     status: "ok",
-    message: "Hello, Server!",
+    message: "Hello, World!",
     uptime: process.uptime(),
   });
 });
